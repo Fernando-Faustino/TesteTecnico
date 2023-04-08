eaa72edb8f41f6ac478899514c933cea58faf3b5
Fernando Faustino <Fernandofaustino627@gmail.com>
2023-04-08 19:14:47 -0300

    Adicionar arquivos de projeto.

---
+++ TesteTecnico/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace testeTecnico {
+    class Program {
+        static void Main(string[] args) {
+
+           
+            int indice = 13, soma = 0, K = 0;
+
+            while (K < indice) {
+                K = K + 1;
+                soma = soma + K;
+            }
+
+            Console.WriteLine(soma);
+
+        }
+    }
+}
+ 
+
+    
